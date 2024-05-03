4ec941e571f26a5124acbc9ab35eccc0a186b729
TaleHTT <136677884+TaleHTT@users.noreply.github.com>
2024-05-03 21:31:12 +0800

    temp

---
+++ Roguelike/Assets/_Script/CoinSetting.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/CoinSettingScriptableObject", order = 1)]
+public class CoinSetting : ScriptableObject
+{
+    public int SumOflayers;
+}
+
+
